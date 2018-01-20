65c70b4afc59b5f0d8285d0be4ab80f773367322
Omiko-chan <torm-torin@mail.ru>
2018-01-21 01:07:09 +0300

    Задание №4: Сделать всё то же самое, но для контактов

---
+++ addressbook-web-tests/addressbook-web-tests/ContactCreationTests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.UseLegacyImplementation = true;
+            options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox ESR\firefox.exe";
+            driver = new FirefoxDriver(options);
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ContactCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            InitContactCreation();
+            ContactData contact = new ContactData("Usernametest", "Usersurnametest");
+            contact.BirthdayDay = "11";
+            contact.BirthdayMonth = "May";
+            contact.BirthdayYear = "1987";
+            FillContactForm(contact);
+            SubmitContactCreation();
+            ReturnToHomePage();
+            Logout();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "addressbook/");
+        }
+ 
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+         private void InitContactCreation()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        private void FillContactForm(ContactData contact)
+        {
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText(contact.BirthdayDay);
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText(contact.BirthdayMonth);
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys(contact.BirthdayYear);
+         }
+
+        private void SubmitContactCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void ReturnToHomePage()
+        {
+            driver.FindElement(By.LinkText("home page")).Click();
+        }
+
+        private void Logout()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+ 
+     private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        private string middlename = "";
+        private string nickname = "";
+        private string title = "";
+        private string company = "";
+        private string address = "";
+        private string telephoneHome = "";
+        private string telephoneMobile = "";
+        private string telephoneWork = "";
+        private string telephoneFax = "";
+        private string email = "";
+        private string email2 = "";
+        private string email3 = "";
+        private string homepage = "";
+        private string birthdayDay = "";
+        private string birthdayMonth = "";
+        private string birthdayYear = "";
+        private string anniversaryDay = "";
+        private string anniversaryMonth = "";
+        private string anniversaryYear = "";
+        private string new_group = "";
+        private string address2 = "";
+        private string phone2 = "";
+        private string notes = "";
+
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+        public string Middlename
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+        public string NickName
+        {
+            get
+            {
+                return nickname;
+            }
+            set
+            {
+                nickname = value;
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        public string Company
+        {
+            get
+            {
+                return company;
+            }
+            set
+            {
+                company = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+        public string TelephoneHome
+        {
+            get
+            {
+                return telephoneHome;
+            }
+            set
+            {
+                telephoneHome = value;
+            }
+        }
+        public string TelephoneMobile
+        {
+            get
+            {
+                return telephoneMobile;
+            }
+            set
+            {
+                telephoneMobile = value;
+            }
+        }
+        public string TelephoneWork
+        {
+            get
+            {
+                return telephoneWork;
+            }
+            set
+            {
+                telephoneWork = value;
+            }
+        }
+        public string TelephoneFax
+        {
+            get
+            {
+                return telephoneFax;
+            }
+            set
+            {
+                telephoneFax = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string Email2
+        {
+            get
+            {
+                return email2;
+            }
+            set
+            {
+                email2 = value;
+            }
+        }
+        public string Email3
+        {
+            get
+            {
+                return email3;
+            }
+            set
+            {
+                email3 = value;
+            }
+        }
+        public string Homepage
+        {
+            get
+            {
+                return homepage;
+            }
+            set
+            {
+                homepage = value;
+            }
+        }
+        public string BirthdayDay
+        {
+            get
+            {
+                return birthdayDay;
+            }
+            set
+            {
+                birthdayDay = value;
+            }
+        }
+        public string BirthdayMonth
+        {
+            get
+            {
+                return birthdayMonth;
+            }
+            set
+            {
+                birthdayMonth = value;
+            }
+        }
+        public string BirthdayYear
+        {
+            get
+            {
+                return birthdayYear;
+            }
+            set
+            {
+                birthdayYear = value;
+            }
+        }
+        public string AnniversaryDay
+        {
+            get
+            {
+                return anniversaryDay;
+            }
+            set
+            {
+                anniversaryDay = value;
+            }
+        }
+        public string AnniversaryMonth
+        {
+            get
+            {
+                return anniversaryMonth;
+            }
+            set
+            {
+                anniversaryMonth = value;
+            }
+        }
+        public string AnniversaryYear
+        {
+            get
+            {
+                return anniversaryYear;
+            }
+            set
+            {
+                anniversaryYear = value;
+            }
+        }
+        public string NewGroup
+        {
+            get
+            {
+                return new_group;
+            }
+            set
+            {
+                new_group = value;
+            }
+        }
+        public string Address2
+        {
+            get
+            {
+                return address2;
+            }
+            set
+            {
+                address2 = value;
+            }
+        }
+        public string Phone2
+        {
+            get
+            {
+                return phone2;
+            }
+            set
+            {
+                phone2 = value;
+            }
+        }
+        public string Notes
+        {
+            get
+            {
+                return notes;
+            }
+            set
+            {
+                notes = value;
+            }
+        }
+    }
+    
+
+}
