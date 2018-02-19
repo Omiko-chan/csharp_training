187ffa81b0b93d6a174b215d165df972e1c9780e
VSK\kovalkina <kovalkina@vsk.ru>
2018-02-19 22:16:04 +0300

    Задание №11: реализовать тесты для проверки информации на главной странице

--- addressbook-web-tests/addressbook-web-tests/Model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/Model/ContactData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WebAddressbookTests
@@ -128,7 +129,7 @@ namespace WebAddressbookTests
             {
                 return "";
             }
-            return phone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "") + "\r\n";
+            return Regex.Replace(phone, "[ ()-]", "") + "\r\n";
         }
     }
 }
--- addressbook-web-tests/addressbook-web-tests/Tests/ContactInformationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactInformationTests.cs
@@ -10,6 +10,25 @@ namespace WebAddressbookTests
     [TestFixture]
     public class ContactInformationTests : AuthTestBase
     {
+        [SetUp]
+        public void PreconditionsContactModif()
+        {
+            ContactData contactData = new ContactData("Usersurname", "Username")
+            {
+                BirthdayDay = "22",
+                BirthdayMonth = "October",
+                BirthdayYear = "1990",
+                PhoneHome="+7 (567) 786-78-89",
+                PhoneMobile="8 976-67-98",
+                PhoneWork = "(495)256-56-65",
+                Email="emailone@mail.ru",
+                Email2="email.two@gmail.com",
+                Email3="email-three@yandex.ru"
+            };
+            appManager.Contact.PreconditionsContact(contactData);
+        }
+
+
         [Test]
         public void ContactInformationTest()
         {
