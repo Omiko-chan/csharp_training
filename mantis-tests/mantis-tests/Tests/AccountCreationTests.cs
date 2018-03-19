73f9b236a8172e4d88a92d56cda753344da53c57
Omiko-chan <torm-torin@mail.ru>
2018-03-19 23:33:45 +0300

    Подмена конфига для отключения капчи

---
+++ mantis-tests/mantis-tests/Tests/AccountCreationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO;
+
+namespace mantis_tests
+{
+    [TestFixture]
+    public class AccountCreationTests : TestBase
+    {
+        [OneTimeSetUp]
+        public void setUpConfig()
+        {
+            Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
+            appManager.Ftp.BackupFile(@"/config_inc.php");
+            using (Stream localFile = File.Open("config_inc.php",FileMode.Open))
+            { appManager.Ftp.Upload(@"/config_inc.php", null); }
+        }
+        [Test]
+        public void TestAccountRegistration()
+        {
+            AccountData account = new AccountData()
+            {
+                Name="testuser",
+                Password="password",
+                Email= "testuser@localhost.localdomain"
+            };
+            appManager.Registration.Register(account);
+        }
+
+        [OneTimeTearDown]
+        public void restoreConfig()
+        {
+            Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
+            appManager.Ftp.RestoreBackupFile(@"/config_inc.php");
+
+        }
+    }
+}
